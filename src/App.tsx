2ce3ef0893c7c67345b1362519b8c8cef85ca04b
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2023-11-29 17:01:35 +0100

    Add consultation 2023-11-29

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Lesson20 from "./lessons/Lesson20";
+// Lessons
+// import Lesson20 from "./lessons/Lesson20";
+
+//Consultations
+import Counsultation20 from "./consultations/Consultation20";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Lesson20 />
+      {/* Lessons */}
+      {/* <Lesson20 /> */}
+
+      {/* Consultations */}
+      <Counsultation20 />
     </div>
   );
 }
---
+++ src/consultations/Consultation20/index.ts
@@ -0,0 +1,3 @@
+import Counsultation20 from "./Counsultation20";
+
+export default Counsultation20;
